3616c37911e67349af5573e42c06249ce4721aeb
Miriam <miriam.martin@roiback.com>
2020-11-30 21:25:04 +0100

    feat: update storybook config and preview

--- .storybook/config.js
+++
@@ -1,9 +0,0 @@
-import { configure } from '@storybook/react';
-
-const req = require.context('../src/components', true, /\.stories\.js$/)
-
-function loadStories() {
-  req.keys().forEach(fileName => req(fileName));
-}
-
-configure(loadStories, module);
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,35 @@
 
+import { ThemeProvider } from 'styled-components';
+import { withThemes } from '@react-theming/storybook-addon';
+
+import theme from '../src/themes/theme';
+import alternativeTheme from '../src/themes/alternativeTheme';
+
+const themes = [
+    {
+      name: 'Theme1', // Required it's used for displaying the button label,
+      backgroundColor: '#fff', // Optional, it's used for setting dynamic background color on storybook
+      ...theme,
+    },
+    // TODO: update values
+    // {
+    //   name: 'Theme2', // Required it's used for displaying the button label,
+    //   backgroundColor: '#000',// Optional, it's used for setting dynamic background color on storybook
+    //   ...alternativeTheme, // Your theme keys (Check example if you need some help)
+    // }
+];
+
+export const decorators = [withThemes(ThemeProvider, themes)];
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: {
+        argTypesRegex: "^on[A-Z].*"
+    },
+    options: {
+        // order: ['Tokens', 'Layout', 'Components', 'Plugins'],
+        method: 'alphabetical',
+    },
+    controls: {
+        expanded: true
+    },
 }
